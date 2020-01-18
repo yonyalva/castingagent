2a1a26220aa8fb85111f94fd4be3dfb0b6d2e285
yonyalva <alexjalv01@yahoo.com>
2020-01-17 21:43:52 -0500

    first push

---
+++ precache-manifest.32755ac46e986e734a2a4506f935254d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff1056d11916a7e62e20",
+    "url": "/reactcats/static/css/main.9cadc3a9.chunk.css"
+  },
+  {
+    "revision": "ff1056d11916a7e62e20",
+    "url": "/reactcats/static/js/main.ff1056d1.chunk.js"
+  },
+  {
+    "revision": "606fc51d7995ea135174",
+    "url": "/reactcats/static/js/1.606fc51d.chunk.js"
+  },
+  {
+    "revision": "95cea89e41278da3e020",
+    "url": "/reactcats/static/js/runtime~main.95cea89e.js"
+  },
+  {
+    "revision": "70ceb1601c6ee6f20ee3a08065a28c80",
+    "url": "/reactcats/index.html"
+  }
+];
